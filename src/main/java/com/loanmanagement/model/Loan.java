c1dd74c9b0beacf274553b1ed56e570f78943a8e
akhil-ekkelly <ekkellyakhil@gmail.com>
2026-09-22 21:47:04 +0530

    getters and setters and tostring() for Model

--- src/main/java/com/loanmanagement/model/Loan.java
+++ src/main/java/com/loanmanagement/model/Loan.java
@@ -13,4 +13,119 @@ public class Loan {
     private String startDate;
     private String status;
     private int createdBy;
+
+    public int getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(int loanId) {
+        this.loanId = loanId;
+    }
+
+    public int getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(int applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getLoanTypeId() {
+        return loanTypeId;
+    }
+
+    public void setLoanTypeId(int loanTypeId) {
+        this.loanTypeId = loanTypeId;
+    }
+
+    public double getPrincipalAmount() {
+        return principalAmount;
+    }
+
+    public void setPrincipalAmount(double principalAmount) {
+        this.principalAmount = principalAmount;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public int getTenureMonths() {
+        return tenureMonths;
+    }
+
+    public void setTenureMonths(int tenureMonths) {
+        this.tenureMonths = tenureMonths;
+    }
+
+    public double getTotalPayable() {
+        return totalPayable;
+    }
+
+    public void setTotalPayable(double totalPayable) {
+        this.totalPayable = totalPayable;
+    }
+
+    public double getOutstandingAmount() {
+        return outstandingAmount;
+    }
+
+    public void setOutstandingAmount(double outstandingAmount) {
+        this.outstandingAmount = outstandingAmount;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Loan{" +
+                "loanId=" + loanId +
+                ", applicationId=" + applicationId +
+                ", customerId=" + customerId +
+                ", loanTypeId=" + loanTypeId +
+                ", principalAmount=" + principalAmount +
+                ", interestRate=" + interestRate +
+                ", tenureMonths=" + tenureMonths +
+                ", totalPayable=" + totalPayable +
+                ", outstandingAmount=" + outstandingAmount +
+                ", startDate='" + startDate + '\'' +
+                ", status='" + status + '\'' +
+                ", createdBy=" + createdBy +
+                '}';
+    }
 }
--- src/main/java/com/loanmanagement/model/LoanType.java
+++ src/main/java/com/loanmanagement/model/LoanType.java
@@ -9,4 +9,83 @@ public class LoanType {
     private double maxAmount;
     private int maxTenureMonths;
     private String status;
+
+
+    public int getLoanTypeId() {
+        return loanTypeId;
+    }
+
+    public void setLoanTypeId(int loanTypeId) {
+        this.loanTypeId = loanTypeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public double getMinAmount() {
+        return minAmount;
+    }
+
+    public void setMinAmount(double minAmount) {
+        this.minAmount = minAmount;
+    }
+
+    public double getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(double maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public int getMaxTenureMonths() {
+        return maxTenureMonths;
+    }
+
+    public void setMaxTenureMonths(int maxTenureMonths) {
+        this.maxTenureMonths = maxTenureMonths;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "LoanType{" +
+                "loanTypeId=" + loanTypeId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", interestRate=" + interestRate +
+                ", minAmount=" + minAmount +
+                ", maxAmount=" + maxAmount +
+                ", maxTenureMonths=" + maxTenureMonths +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
