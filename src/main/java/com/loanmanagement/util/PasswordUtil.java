92c13e31579c078cdc71981facd39018044fa487
akhil-ekkelly <ekkellyakhil@gmail.com>
2026-09-24 22:46:27 +0530

    Password util

--- src/main/java/com/loanmanagement/util/PasswordUtil.java
+++ src/main/java/com/loanmanagement/util/PasswordUtil.java
@@ -1,4 +1,17 @@
 package com.loanmanagement.util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 public class PasswordUtil {
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password", e);
+        }
+    }
 }
